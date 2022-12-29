3a2efe4dd84cbc7337d28fe7ca6acb5292091549
thebreadishard <112763215+thebreadishard@users.noreply.github.com>
2022-12-29 17:39:03 +0100

    more functions

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -52,7 +52,7 @@ const cart = [];
 */
 
 function addProductToCart(productId) {
-  const product = products.find((product) => product.productId === productId);
+  let product = products.find((product) => product.productId === productId);
   product.quantity = product.quantity + 1;
   if (!cart.includes(product)) {
     cart.push(product);
@@ -64,17 +64,35 @@ function addProductToCart(productId) {
   - increaseQuantity should then increase the product's quantity
 */
 
+function increaseQuantity(productId) {
+  let product = products.find((product) => product.productId === productId);
+  product.quantity = product.quantity + 1;
+}
+
 /* Create a function named decreaseQuantity that takes in the productId as an argument
   - decreaseQuantity should get the correct product based on the productId
   - decreaseQuantity should decrease the quantity of the product
   - if the function decreases the quantity to 0, the product is removed from the cart
 */
 
+function decreaseQuantity(productId) {
+  let product = products.find((product) => product.productId === productId);
+  product.quantity = product.quantity - 1;
+}
+
 /* Create a function named removeProductFromCart that takes in the productId as an argument
   - removeProductFromCart should get the correct product based on the productId
   - removeProductFromCart should update the product quantity to 0
   - removeProductFromCart should remove the product from the cart
 */
+
+function removeProductFromCart(productId) {
+  let product = products.find((product) => product.productId === productId);
+  product.quantity = product.quantity + 1;
+  if (cart.includes(product)) {
+    cart.pop(product);
+  };
+}
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
